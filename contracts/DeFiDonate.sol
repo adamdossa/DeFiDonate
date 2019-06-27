fa01cb54b3dcb9783d3b85265bab98e9d1e0cf41
Adam Dossa <adam.dossa@gmail.com>
2019-06-27 18:17:52 -0400

    Fixes

--- contracts/DeFiDonate.sol
+++ contracts/DeFiDonate.sol
@@ -57,7 +57,7 @@ contract DeFiDonate is ERC20, ERC20Detailed {
     {
         require(_charity != address(0));
         require(_coolOffLength < _epochLength);
-        governanceToken = new GovernanceToken(concat(_name, " - Voting"), concat(_symbol, "_VOTE", 18));
+        governanceToken = new GovernanceToken(concat(_name, " - Voting"), concat(_symbol, "_VOTE"), 18);
         depositToken = ERC20(_depositToken);
         compound = ICompound(_compound);
         charity = _charity;
@@ -66,14 +66,14 @@ contract DeFiDonate is ERC20, ERC20Detailed {
         nextEpoch = block.timestamp.add(epochLength);
     }
 
-    function concat(string _a, string _b) public pure returns (string){
+    function concat(string memory _a, string memory _b) public pure returns (string memory) {
         bytes memory bytes_a = bytes(_a);
         bytes memory bytes_b = bytes(_b);
         string memory length_ab = new string(bytes_a.length + bytes_b.length);
         bytes memory bytes_c = bytes(length_ab);
         uint k = 0;
         for (uint i = 0; i < bytes_a.length; i++) bytes_c[k++] = bytes_a[i];
-        for (i = 0; i < bytes_b.length; i++) bytes_c[k++] = bytes_b[i];
+        for (uint i = 0; i < bytes_b.length; i++) bytes_c[k++] = bytes_b[i];
         return string(bytes_c);
     }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,5 @@ const MockDepositToken = artifacts.require("MockDepositToken.sol");
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(MockDepositToken);
-  await deployer.deploy(DeFiDonate, "GDAI", "GDAI", 18, "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea", "0x6D7F0754FFeb405d23C51CE938289d4835bE3b14", "0x85523D0f76B3A6C3c05b2CfBb0558B45541f100B", 100, {from: accounts[0]});
+  await deployer.deploy(DeFiDonate, "Charitable DAI", "CHARITY_DAI", 18, "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea", "0x6D7F0754FFeb405d23C51CE938289d4835bE3b14", "0x85523D0f76B3A6C3c05b2CfBb0558B45541f100B", 60 * 60 * 24 * 30, 60 * 60 * 24 * 7, {from: accounts[0]});
 };
